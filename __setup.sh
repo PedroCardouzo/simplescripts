ba9b3a43cacc00d6717d83805d57ec82f98c89fb
Pedro Cardouzo <pedro.cardouzo@gmail.com>
2018-10-05 23:32:15 -0300

    changed __setup and __update scripts

--- __setup.sh
+++ __setup.sh
@@ -1,3 +1,15 @@
-#!/bin/sh
+#!/bin/bash
 
-echo 'main'
+function __create_or_update_files {	
+	if [ -d "$1" ]; then
+		cd "$1";
+		chmod +x __update.sh;
+		./__update.sh "$2";
+		cd ../;
+	else
+		git clone "https://github.com/PedroCardouzo/""$1"".git";
+	fi
+}
+
+__create_or_update_files "simplescripts" "$1";
+__create_or_update_files "xml_decoder" "$1";
